d1976072f50a48a0aeef65a31297f6b169b2fb74
BenilsonFalcao <benilson.25104708360010@faeterj-rio.edu.br>
2025-11-18 22:36:01 -0300

    Update and rename README.md to inicio.php

---
+++ inicio.php
@@ -0,0 +1,7 @@
+<?php
+// ALERTA: Em ambiente de produção, não use 'root' e senha vazia!
+$db_host = "localhost";
+$db_user = "root"; 
+$db_pass = ""; 
+$db_name = "petshop"; // Mantenho o nome original, mas idealmente seria 'monitoramento'
+?>
